0a4e16349e282e37bbc7c756113ea1dd866fb96e
Pipe Galera <pipegalera@gmail.com>
2024-09-28 22:51:18 +0200

    Fixed refresh process.

--- src/data_refresh.py
+++ src/data_refresh.py
@@ -8,7 +8,7 @@ load_dotenv()
 def run_refresh():
     old_data = pd.read_parquet("./data/data.parquet")
     print("--> Rows before:",  f"{len(old_data):,}")
-    print("--> Current data updated until:", old_data.period.max())
+    print("--> Current data updated until:", old_data.period.max().strftime("%Y-%m-%dT%H"))
 
     EIA_API_KEY = os.getenv('EIA_API_KEY')
     API_PATH = "electricity/rto/region-sub-ba-data/"
@@ -16,21 +16,19 @@ def run_refresh():
         'parent': ['CISO'],
         'subba': ['PGAE', 'SCE', 'SDGE', 'VEA'],
     }
-    last_date = pd.to_datetime(old_data['period'].max())
-    start = (last_date + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
-    end = datetime.now(timezone.utc)
+    last_date = old_data['period'].max().tz_convert('UTC')
+    start = (last_date + timedelta(hours=1))
 
     new_data = eia_backfill_data(api_key=EIA_API_KEY,
                             api_path=API_PATH,
                             start = start,
-                            end = end,
                             length = 5000,
                             frequency = "hourly",
                             facets=facets,
                             refresh=True)
 
     print("--> Rows after:", f"{len(new_data):,}")
-    print("--> New Data Updated until:", new_data.period.max())
+    print("--> New Data Updated until:", new_data.period.max().strftime("%Y-%m-%dT%H"))
 
 if __name__=="__main__":
     run_refresh()
