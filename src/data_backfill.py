000d63b0ffea692ebb9d13d45c6813ef1780a0a7
Pipe Galera <pipegalera@gmail.com>
2024-09-28 22:50:39 +0200

    Fixed api bug to retrieve data duplicated

--- src/data_backfill.py
+++ src/data_backfill.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 EIA_API_KEY = os.getenv('EIA_API_KEY')
-DATA_PATH = "/Users/pipegalera/dev/Documents/GitHub/EIA_tracker/data/data.parquet"
 API_PATH = "electricity/rto/region-sub-ba-data/"
 facets = {
     'parent': ['CISO'],
@@ -20,53 +19,53 @@ def format_facets(facets_dict):
     return formatted_facets
 
 def eia_get_data(api_key,
-            api_path,
-            start = None,
-            end = None,
-            length = None,
-            offset = 0,
-            frequency = None,
-            facets = None):
+                 api_path,
+                 start=None,
+                 end=None,
+                 length=None,
+                 offset=0,
+                 frequency=None,
+                 facets=None):
 
+    start = pd.to_datetime(start, utc=True).strftime("%Y-%m-%dT%H")
+    print("--> Retrieving data from:", start)
 
-    start = pd.to_datetime(start).strftime("%Y-%m-%dT%H:%M:%S")
-    end   = pd.to_datetime(end).strftime("%Y-%m-%dT%H:%M:%S")
     api_path = api_path if api_path.endswith("/") else api_path + "/"
     url = "https://api.eia.gov/v2/" + api_path + 'data/'
 
     parameters = {
-        'api_key': api_key,
         'frequency': frequency,
-        'offset': offset,
-        'data[]': 'value',
+        'data[0]': 'value',
         'start': start,
-        'end': end,
+        'sort[0][column]': 'period',
+        'sort[0][direction]': 'desc',
+        'offset': offset,
         'length': length,
+        'api_key': api_key,
     }
 
     if facets:
-        parameters = {**parameters, **format_facets(facets)}
+        parameters.update(format_facets(facets))
 
-    # print(requests.get(url, params=parameters).url)
-    # print(requests.get(url, params=parameters))
+    #print(requests.get(url, params=parameters).url)
+    #print(requests.get(url, params=parameters))
 
     response = requests.get(url, params=parameters).json()
 
+    df = pd.DataFrame(response['response']['data'])
 
-    if len(response) <= 2:
-    # len=2 == {'error': 'Call to a member function format() on bool', 'code': 0}
+    if df is None or df.empty:
         return None
     else:
-        df = pd.DataFrame(response['response']['data'])
         df["value"] = pd.to_numeric(df["value"])
-        df = df.sort_values(by = [df.columns[0],df.columns[3], df.columns[1]])
-        return df
-
+        df["period"] = pd.to_datetime(df["period"], utc=True)
+        df = df[df["period"] > start] # For safety, the api retrieves data lazily (without caring the hours)
+        df = df.sort_values(by=[df.columns[0], df.columns[3], df.columns[1]])
+    return df
 
 def eia_backfill_data(api_key,
                 api_path,
                 start = None,
-                end = None,
                 length = None,
                 offset = 0,
                 frequency = None,
@@ -79,19 +78,18 @@ def eia_backfill_data(api_key,
             df = eia_get_data(api_key = api_key,
                               api_path = api_path,
                               start = start,
-                              end = end,
                               length = length,
                               frequency = frequency,
                               facets = facets,
                               offset = offset)
 
             if df is None or df.empty:
-                        break
+                break
 
             all_data.append(df)
             offset += length
 
-            print(f"Retrieved {len(df)} rows. Total rows so far: {sum(len(d) for d in all_data)}")
+            print(f"Retrieved {len(df)} datapoints. Total datapoints so far: {sum(len(d) for d in all_data)}")
 
             # Print the last row of the most recently appended data
             # print("Last row of the most recent data:")
@@ -102,7 +100,7 @@ def eia_backfill_data(api_key,
         if not all_data:
             print("--> No new data found in the API")
             df = pd.read_parquet("./data/data.parquet")
-        else:
+        else: # else = there is new data
             df = pd.concat([
                 pd.read_parquet("./data/data.parquet"),
                 pd.concat(all_data, ignore_index=True)
@@ -110,8 +108,7 @@ def eia_backfill_data(api_key,
             df.to_parquet("./data/data.parquet", index=False)
     else: # else = new backfill
         df = pd.concat(all_data, ignore_index=True)
-
-    #print("Data saved under:", DATA_PATH)
+        df.to_parquet("./data/data.parquet", index=False)
 
     return df
 
@@ -120,7 +117,6 @@ if __name__== "__main__":
     eia_backfill_data(api_key=EIA_API_KEY,
                             api_path=API_PATH,
                             start = '2019-01-01',
-                            end = '2024-01-01',
                             length = 5000,
                             frequency = "hourly",
                             facets=facets,
