407a566c98e8e4c1b77e944237c91001a81073ef
Pipe Galera <pipegalera@gmail.com>
2024-10-08 22:11:11 +0200

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+
+def complete_timeframe(data, bfill=False):
+
+    df = data.copy()
+
+    df["period"] = df["period"].dt.tz_localize(None)
+    date_range = pd.date_range(start=df.period.min(),
+                        end=df.period.max(),
+                        freq='h')
+    df_combined = pd.DataFrame()
+    for subba in df.subba.unique():
+        df_subba = pd.DataFrame({"period": date_range, "subba": subba})
+        df_combined = pd.concat([df_combined, df_subba])
+
+    if len(df_combined) == len(date_range)*4:
+        df = df_combined.merge(df, on=['period', 'subba'], how='left')
+    if bfill==True:
+        df['value'] = df.groupby('subba')['value'].bfill()
+
+    return df
+def create_group_lags(data, group_column, target_columns, lags):
+
+    df = data.copy()
+    for col in target_columns:
+        for lag in lags:
+            df[f'{col}_lag_{lag}'] = df.groupby(group_column)[col].shift(lag)
+        return df
+def create_group_rolling_means(data, group_column, target_columns, windows):
+
+    df = data.copy()
+
+    df = df.sort_values(by=[group_column]).reset_index(drop=True)
+    for col in target_columns:
+        for window in windows:
+            new_col_name = f'{col}_rolling_mean_{window}_hours'
+            df[new_col_name] = (
+                df.groupby(group_column)[col]
+                .rolling(window=window, min_periods=1)
+                .mean()
+                .reset_index(level=0, drop=True)
+            )
+
+    return df
+def create_date_colums(data, date_column):
+
+    df = data.copy()
+
+    df[f'{date_column}_day'] = df[date_column].dt.day
+    df[f'{date_column}_week'] = df[date_column].dt.isocalendar().week.astype('int32')
+    df[f'{date_column}_year'] = df[date_column].dt.year
+    df[f'{date_column}_day_of_week'] = df[date_column].dt.dayofweek
+    df[f'{date_column}_day_of_year'] = df[date_column].dt.dayofyear
+    df[f'{date_column}_month_end'] = df[date_column].dt.is_month_end
+    df[f'{date_column}_month_start'] = df[date_column].dt.is_month_start
+    df[f'{date_column}_quarter_end'] = df[date_column].dt.is_quarter_end
+    df[f'{date_column}_quarter_start'] = df[date_column].dt.is_quarter_start
+    df[f'{date_column}_year_end'] = df[date_column].dt.is_quarter_end
+    df[f'{date_column}_year_start'] = df[date_column].dt.is_quarter_start
+
+    return df
+def test_stationarity(timeseries):
+    result = adfuller(timeseries, autolag='AIC')
+    if result[1] <= 0.01:
+        return  print("Augmented Dickey-Fuller unit root test shows clear stationality")
+    else:
+        return print("Stationality cannot be proven at 1%")
