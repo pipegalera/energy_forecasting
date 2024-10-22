81b402a21ab2a784ce51f116a2f64047f956e453
Pipe Galera <pipegalera@gmail.com>
2024-10-22 23:07:52 +0200

    Improved mlflow experiment

--- model_training/xgboost_training.py
+++ model_training/xgboost_training.py
@@ -5,7 +5,7 @@ import sys
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error, mean_absolute_percentage_error
 from sklearn.model_selection import TimeSeriesSplit
 import mlflow
 import mlflow.xgboost
@@ -19,36 +19,37 @@ MODELS_PATH = os.getenv("MODELS_PATH")
 os.chdir(HOME_PATH)
 sys.path.append(os.getcwd())
 
-from src.utils import complete_timeframe, create_group_lags, create_group_rolling_means, create_date_colums
+from src.utils import complete_timeframe, create_group_lags, create_group_rolling_means, create_date_columns
+
+
 
 # SET EXPERIMENT
-mlflow_dir = "mlflow"
-if not os.path.exists(mlflow_dir):
-    os.makedirs(mlflow_dir)
-mlflow.set_tracking_uri(f"sqlite:///{mlflow_dir}/mlflow.db")
-mlflow.set_experiment("XGBoost_Energy_Forecasting")
-print("MLflow tracking URI:", mlflow.get_tracking_uri())
-# mlflow ui --backend-store-uri sqlite:///mlflow/mlflow.db
+# bash -> mlflow server --host 127.0.0.1 --port 8080
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+experiment_name = "XGBoost_Energy_Forecasting"
+mlflow.set_experiment(experiment_name)
+
 
 # READ DATA
 df = pd.read_parquet(DATA_PATH + "data.parquet")
 
 # PREP DATA
-df = (df.pipe(complete_timeframe, bfill=True)
+df = (df
+        .pipe(complete_timeframe, bfill=True)
         .pipe(create_group_lags, 'subba', ['value'], lags=[3,6,12,24,48,168,336,720,2160])
         .pipe(create_group_rolling_means, 'subba', ['value'], windows=[3,6,12,24,48,168,336,720,2160])
-        .pipe(create_date_colums, 'period')
+        .pipe(create_date_columns, 'period')
      )
 df = df.sort_values(['subba', 'period'])
 
 
 # Covariates
-covs = ['value_lag_3', 'value_lag_6', 'value_lag_12',
-       'value_lag_24', 'value_lag_48', 'value_lag_168', 'value_lag_336',
-       'value_lag_720', 'value_lag_2160', 'value_rolling_mean_3_hours',
-       'value_rolling_mean_6_hours', 'value_rolling_mean_12_hours',
-       'value_rolling_mean_24_hours', 'value_rolling_mean_48_hours',
-       'value_rolling_mean_168_hours', 'value_rolling_mean_336_hours',
+covs = ['value_lag_3_hours', 'value_lag_6_hours', 'value_lag_12_hours',
+       'value_lag_24_hours', 'value_lag_48_hours', 'value_lag_168_hours',
+       'value_lag_336_hours', 'value_lag_720_hours', 'value_lag_2160_hours',
+       'value_rolling_mean_3_hours', 'value_rolling_mean_6_hours',
+       'value_rolling_mean_12_hours', 'value_rolling_mean_24_hours', 'value_rolling_mean_48_hours','value_rolling_mean_168_hours',
+       'value_rolling_mean_336_hours',
        'value_rolling_mean_720_hours', 'value_rolling_mean_2160_hours',
        'period_hour', 'period_day', 'period_week', 'period_year',
        'period_day_of_week', 'period_day_of_year', 'period_month_end',
@@ -73,7 +74,8 @@ def objective(trial, data, group, target, covs, n_splits=5):
     tss = TimeSeriesSplit(n_splits=n_splits, test_size=24*365*1, gap=24)
     df_group = data[(data["subba"] == group)].set_index("period").sort_index()
 
-    scores = []
+    rmse_scores = []
+    mape_scores = []
     for i, (train_idx, test_idx) in enumerate(tss.split(df_group)):
         train = df_group.iloc[train_idx]
         test = df_group.iloc[test_idx]
@@ -87,25 +89,29 @@ def objective(trial, data, group, target, covs, n_splits=5):
                 )
 
         y_pred = model.predict(X_test)
-        score = root_mean_squared_error(y_test, y_pred)
-        scores.append(score)
 
-    return np.mean(scores)
+        rmse_score = root_mean_squared_error(y_test, y_pred)
+        mape_score = mean_absolute_percentage_error(y_test, y_pred)
+        rmse_scores.append(rmse_score)
+        mape_scores.append(mape_score)
+
+    return np.mean(rmse_scores), np.mean(mape_scores)
 
 def optimize_and_train(data, group, target, covs, n_splits=5, n_trials=100):
-    with mlflow.start_run(run_name=f"XGBoost_Optuna_{group}") as run:
+    with mlflow.start_run(run_name=f"XGBoost_Optuna_{group}",nested=True) as run:
         mlflow.log_param("group", group)
         mlflow.log_param("n_splits", n_splits)
         mlflow.log_param("n_trials", n_trials)
 
-        study = optuna.create_study(direction='minimize')
+        study = optuna.create_study(directions=['minimize', 'minimize'])
         study.optimize(lambda trial: objective(trial, data, group, target, covs, n_splits), n_trials=n_trials)
 
-        best_params = study.best_params
-        best_value = study.best_value
+        best_params = study.best_trials[-1].params
+        best_rmse, best_mape = study.best_trials[-1].values
 
         mlflow.log_params(best_params)
-        mlflow.log_metric("best_RMSE", best_value)
+        mlflow.log_metric("best_RMSE", best_rmse)
+        mlflow.log_metric("best_MAPE", best_mape)
 
         # Train final model with best params
         best_params['objective'] = 'reg:squarederror'
@@ -119,6 +125,11 @@ def optimize_and_train(data, group, target, covs, n_splits=5, n_trials=100):
         mlflow.xgboost.log_model(best_model,
                                  f"xgboost_model_{group}",
                                  input_example=X.iloc[:5])
+
+        # Calculate and log MAPE for the entire dataset
+        y_pred = best_model.predict(X)
+        final_mape = mean_absolute_percentage_error(y, y_pred)
+        mlflow.log_metric("final_MAPE", final_mape)
 
         # Save run id
         run_id = run.info.run_id
@@ -134,6 +145,8 @@ def optimize_and_train(data, group, target, covs, n_splits=5, n_trials=100):
 
         with open(mapping_file, 'w') as f:
             json.dump(run_id_mapping, f)
+
+    mlflow.end_run()
 
     return best_model
 
